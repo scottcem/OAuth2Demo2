d69b30970295e409ccc8da58b6240739e7cb4833
scottcem <74527234+scottcem@users.noreply.github.com>
2021-02-09 16:30:50 -0600

    init. commit

---
+++ oauth2Demo2/src/main/java/com/tts/oauth2Demo2/Oauth2Demo2Application.java
@@ -0,0 +1,13 @@
+package com.tts.oauth2Demo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2Demo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2Demo2Application.class, args);
+	}
+
+}
